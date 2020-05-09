ee58cbfc54896dfc12b380f0216fa6723e2afd80
Kire Semada <egadames990@gmail.com>
2020-05-08 17:59:09 -0700

    Fixed Review Error

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
 
 const Navbar2 = (props) => {
   console.log(props.key)
   return (
     <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Flashcard App</Navbar.Brand>
+    <Navbar.Brand href={`/`}>Flashcard App</Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link href={`/flashcard/update/${props.id}`}>View All</Nav.Link>
-      <Nav.Link href={`/flashcard/update/${props.id}`}>Add a Flashcard</Nav.Link>
-      <Nav.Link href={`/delete/${props.id}`}>Review Quiz</Nav.Link>
+      <Nav.Link href={`/flashcard`}>View All</Nav.Link>
+      <Nav.Link href={`/add`}>Add a Flashcard</Nav.Link>
+      <Nav.Link href={`/review`}>Review Quiz</Nav.Link>
     </Nav>
   </Navbar>
   );
